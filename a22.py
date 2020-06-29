935188f864a16611eece6ea9a7f8fe10a41b5d59
gbanfi3 <gbanfi3@gmail.com>
2020-06-29 21:21:41 +0200

    pprint

--- a22.py
+++ a22.py
@@ -1,6 +1,10 @@
+import pprint
 aa = {}
 aa["a"] = list(range(1,11))
 aa["b"] = list(range(11,21))
 aa["c"] = list(range(21,31))
 
-print(aa)
+pprint.pprint(aa)
+
+bb = {"a":list(range(1,11)),"b":list(range(11,21))}
+pprint.pprint(bb)
