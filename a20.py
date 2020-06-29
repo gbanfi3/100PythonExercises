6614b93d0b2666f6a541b34ec44323c01ba8fca8
gbanfi3 <gbanfi3@gmail.com>
2020-06-29 20:41:27 +0200

    values

--- a20.py
+++ a20.py
@@ -3,3 +3,16 @@ summ = 0
 for i in d:
     summ += d[i]
 print(summ)
+
+a = sum([d[i] for i in d])
+print(a)
+
+b = 0
+for i in d.keys():
+    b += d[i]
+print(b)
+
+b = 0
+for i in d.values():
+    b += i
+print(b)
