ad887a0d54cb7219a9de25a87cd36597d7588a6c
gbanfi3 <gbanfi3@gmail.com>
2020-06-30 13:30:28 +0200

    első verzió

--- a29.py
+++ a29.py
@@ -1,7 +1,8 @@
 import math
 
 def volume(hc, r=10):
-    return 4 * math.pi * r**3 /3
+    return 4 * math.pi * r**3 /3 - math.pi * hc**2 * (3*r-hc) / 3
 
-print(3)
-print(3,10)
+print(volume(hc=2))
+print(volume(hc=2,r=10))
+print(volume(2,r=11))
---
+++ a35.py
@@ -0,0 +1,33 @@
+import string
+
+def countwords(s:string):
+    hossz = len(s)
+    if hossz == 0:
+        return 0
+
+    if s[0] in string.ascii_letters:
+        c = 1
+    else:
+        c = 0
+    for i in range(1,hossz):
+        if s[i-1] not in string.ascii_letters and s[i] in string.ascii_letters:
+            c +=1
+    return c
+
+# s = ""
+# s = "A"
+# s = "2"
+# s = "aa"
+# s = "a1a"
+# s = "1a1"
+s = "a1a2a3a4a5"
+print(countwords(s))
+
+
+
+# print(string.whitespace)
+# print(string.ascii_lowercase)
+# print(string.ascii_letters)
+# print(string.digits)
+# print(string.punctuation)
+# print(string.printable)
