e622ff5e310aa5c5af6763ee8ab88a90df7d06d0
gbanfi3 <gbanfi3@gmail.com>
2020-06-30 19:20:25 +0200

    1st ver

--- a43.py
+++ a43.py
@@ -3,9 +3,9 @@ import string
 a = string.ascii_lowercase
 with open("a43.txt", "w") as file:
     hossz = len(a)
-    if hossz %% 2:
+    if hossz % 2:
         a += " "
         hossz = len(a)
-    for n in range(hossz/2):
-        file.write(a[2*n] + a[2*n+1])
+    for n in range(int(hossz/2)):
+        file.write(a[2*n] + a[2*n+1] + "\n")
 
