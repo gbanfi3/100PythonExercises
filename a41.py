83724cf81f4b7efd4e6085d29ed612ccdbe09959
gbanfi3 <gbanfi3@gmail.com>
2020-06-30 18:59:50 +0200

    második verzió

--- a41.py
+++ a41.py
@@ -1,6 +1,6 @@
 import string
 
 with open("a41.txt", "w") as file:
-    for n in range(len(string.ascii_letters)):
-        file.write(string.ascii_letters[n] + "\n")
+    for c in string.ascii_letters:
+        file.write(c + "\n")
 
