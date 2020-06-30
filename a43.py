48200d97d63a2e12e5ff4e0f04940896f9dcb07f
gbanfi3 <gbanfi3@gmail.com>
2020-06-30 19:18:08 +0200

    1st ver

---
+++ a43.py
@@ -0,0 +1,11 @@
+import string
+
+a = string.ascii_lowercase
+with open("a43.txt", "w") as file:
+    hossz = len(a)
+    if hossz %% 2:
+        a += " "
+        hossz = len(a)
+    for n in range(hossz/2):
+        file.write(a[2*n] + a[2*n+1])
+
