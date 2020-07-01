e8d7368438c57ba4dae42f6d0836c2bec3615727
gbanfi3 <gbanfi3@gmail.com>
2020-07-01 16:10:21 +0200

    2nd ver

--- a45.py
+++ a45.py
@@ -1,7 +1,13 @@
-import string
+# import string
+#
+# for i in string.ascii_lowercase:
+#     f = open("./letters2/" + i + ".txt", "w")
+#     # print("letters2/" + i + ".txt")
+#     f.write(i)
+#     f.close()
 
-for i in string.ascii_lowercase:
-    f = open("./letters2/" + i + ".txt", "w")
-    # print("letters2/" + i + ".txt")
-    f.write(i)
-    f.close()
+import os
+
+dirr = "./letters3/"
+if not os.path.exists(dirr):
+    os.mkdir(dirr)
