b5e2fabb6aaabe8dca3f32f466e961efcf3b9709
gbanfi3 <gbanfi3@gmail.com>
2020-07-01 19:44:17 +0200

    1st

--- a53.py
+++ a53.py
@@ -1,7 +1,13 @@
+import pprint
 d = {"employees":[{"firstName": "John", "lastName": "Doe"},
                 {"firstName": "Anna", "lastName": "Smith"},
                 {"firstName": "Peter", "lastName": "Jones"}],
     "owners":[{"firstName": "Jack", "lastName": "Petter"},
             {"firstName": "Jessy", "lastName": "Petter"}]}
 
-print(d["employees"][1]["lastName"])
+# d["employees"][1]["lastName"] = "Smooth"
+
+d["employees"].append({'firstName': 'Albert', 'lastName': 'Bert'})
+
+# pprint.pprint(d["employees"][1]["lastName"])
+pprint.pprint(d)
