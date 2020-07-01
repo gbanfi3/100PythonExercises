6b5c42465feccf180103505da8308ea515a50cac
gbanfi3 <gbanfi3@gmail.com>
2020-07-01 16:06:24 +0200

    1st ver

--- a45.py
+++ a45.py
@@ -1,6 +1,7 @@
 import string
 
 for i in string.ascii_lowercase:
-    f = open("/letters2/" + i + ".txt", "w")
+    f = open("./letters2/" + i + ".txt", "w")
+    # print("letters2/" + i + ".txt")
     f.write(i)
     f.close()
