5e21cf7d29f697b7f5f8bc84c2ea01f11e7d25ed
gbanfi3 <gbanfi3@gmail.com>
2020-07-01 19:35:53 +0200

    1st

---
+++ a49.py
@@ -0,0 +1,9 @@
+# pass2=input("Please enter your password: ")
+
+# age = input("What's your age? ")
+# age_last_year = int(age) - 1
+# print("Last year you were %s." % age_last_year)
+
+# print(type("Hey".replace("ey","i")[-1]))
+# print(type("Hey".replace("ey","i")))
+
